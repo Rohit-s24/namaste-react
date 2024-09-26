2270ca4674fb7d29cca5c6b43db5f92cf54ee29a
UZVIS <uzvisdevelopers@gmail.com>
2024-09-26 15:37:41 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", null, "I am h1 Tag"),
+    React.createElement("h2", null, "I am h2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", null, "I am h1 Tag"),
+    React.createElement("h2", null, "I am h2 Tag"),
+  ]),
+]);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
